44339a247871766800866672d0c8a320f5a379a0
Sima903 <mahatosima2004@gmail.com>
2025-11-30 19:15:16 +0530

    employee list

--- src/components/employee/View.jsx
+++ src/components/employee/View.jsx
@@ -15,7 +15,7 @@ const View = () => {
             Authorization : `Bearer ${localStorage.getItem("token")}`,
           },
         });
-        //console.log(response.data)
+        console.log(response.data)
         if(response.data.success) {
           setEmployee(response.data.employee);
         }
