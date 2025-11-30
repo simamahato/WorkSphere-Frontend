083600284a18b91de28cfe405f039dea7fe8ba6c
simamahato <sima31453@gmail.com>
2025-11-30 20:05:22 +0530

    Initial frontend code

--- src/components/employee/View.jsx
+++ src/components/employee/View.jsx
@@ -15,9 +15,9 @@ const View = () => {
             Authorization : `Bearer ${localStorage.getItem("token")}`,
           },
         });
-        //console.log(response.data)
+        console.log(response.data)
         if(response.data.success) {
-          setEmployee(response.data.employee);
+          setEmployee(response.data.employees);
         }
       } catch(error){
           if(error.response && !error.response.data.success) {
